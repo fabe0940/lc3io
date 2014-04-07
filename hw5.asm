5442338bef6a9437ca4cf6463d816b8f9a0d1cb4
Mason Fabel <mason.fabel@gmail.com>
2014-04-06 19:34:38 -0700

    Initial commit
    Initial commit, complete with a .gitignore, basic documentation,
    and a program template.
    
    * .gitignore - Ignore files generated by the assembler and Vim
      temporary files.
    
    * COPYING - Licensed the project under the WTF public license.
    
    * README - Basic readme with instructions on how to build and run
      the project.
    
    * hw5.asm - Template for the actual source code. Contains a header
      with information about the project and the minimum necessary
      assembler directives.

---
+++ hw5.asm
@@ -0,0 +1,16 @@
+; Mason Fabel
+; 7 April 14
+; CS 150
+; Homework 5
+;
+; Print a welcome message upon startup.
+; Prompt the user for input. If given a
+; 'A', then respond with "Cook Before
+; Serving". If given an 'X', respond
+; with "finished." and halt. In any
+; other case print a '?' and reprompt.
+;
+
+	.ORIG x3000
+; 	Do something here
+	.END
